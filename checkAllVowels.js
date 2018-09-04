741932f456c4d7cc97d138c25de369f570b1a736
anastasia <anastasia.santoso.id@gmail.com>
2018-09-04 14:42:53 +0700

    Release 0

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,22 @@
 
+function CheckVowels(str) {
+  var vowels = 'aiueoAIUEO'
+  var checkers = false;
+
+  for (var i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      checkers = true;
+      CheckVowels(i+1);
+    } else {
+      checkers = false;
+      return checkers;
+    }
+  }
+  return checkers;
+}
+
+console.log(CheckVowels('aiueoAoo')); // true
+console.log(CheckVowels('aiunfjhjdf')); //false
+console.log(CheckVowels('aiuevoooo')); // false
+console.log(CheckVowels('aiunfaaaajhjdf')); //false
+console.log(CheckVowels('aiueoAIUEOaiueoAIUEO')); //true
