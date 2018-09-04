783aa38ea5a01e40fecee649aaa2c22fa098ffde
days88 <yayahidayatullah@rocketmail.com>
2018-09-04 13:39:05 +0800

    Check All Vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
 
+function checkVowels(kata){
+    var hurufVokal = 'aiueo';
+    var jumlahVokal = 0;
+    for(var i = 0;i < kata.length;i++){
+
+        for(var j = 0;j < hurufVokal.length;j++){
+            if(kata[i] === hurufVokal[j]){
+                jumlahVokal++
+            }
+        }
+    }
+    if(jumlahVokal === kata.length ){
+        return true
+    }else{
+        return false
+    }
+}
+console.log(checkVowels('ayam'));
+console.log(checkVowels('uaio'));
